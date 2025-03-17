6a4a4fa248b06243bc9cd9f7017789251ad01378
ernestasgr <ernestasgr02@gmail.com>
2025-03-17 20:12:23 +0200

    Add end-to-end testing setup with Playwright; update pipeline and ignore files

---
+++ e2e-tests/tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Pokedex", () => {
+	test("front page can be opened", async ({ page }) => {
+		await page.goto("");
+		await expect(page.getByText("ivysaur")).toBeVisible();
+		await expect(
+			page.getByText(
+				"Pokémon and Pokémon character names are trademarks of Nintendo."
+			)
+		).toBeVisible();
+	});
+});
